e0e28917a694e8d8def93d7d8af0ba46d5e99f47
Ahmed <omidooo_s7@hotmail.com>
2020-11-08 09:37:51 -0800

    DAL Library

---
+++ Employee/Employee.DAL/Entities/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.DAL.Entities
+{
+   
+   public class Employee
+    {
+        public int EmployeeID { get; set; }
+
+
+        [Required(ErrorMessage ="you Must Enter the Fiirst Name")]
+        [MaxLength(50,ErrorMessage ="First Name Must be 50")]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50,ErrorMessage ="LastName Must be 50")]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage ="Please Enter Your Email")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [Compare("EmailAddress",ErrorMessage ="Email Address is not match")]
+        public string ConfirmEmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password" ,ErrorMessage ="password is not matched")]
+        [Display(Name ="Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+        [DataType(DataType.Url)]
+        public string URL { get; set; }
+
+        public string Gender { get; set; }
+        [DataType(DataType.Currency)]
+        public double Salary { get; set; }
+        [Display(Name = "IS active")]
+        public bool isactive { get; set; }
+        [Display(Name ="IS ALive")]
+        public bool isalive { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        [Display(Name ="Hire Date")]
+        public DateTime HireDate { get; set; }
+
+    }
+}
