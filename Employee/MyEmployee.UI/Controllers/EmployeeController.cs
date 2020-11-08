4ba39d03bc0076da6f1cd96bd11c6b13bdac7140
Ahmed <omidooo_s7@hotmail.com>
2020-11-08 13:31:26 -0800

    Index

--- Employee/MyEmployee.UI/Controllers/EmployeeController.cs
+++ Employee/MyEmployee.UI/Controllers/EmployeeController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Employee.DAL.Repository;
+using Employee.BL.RepositoryClasses;
 
 namespace MyEmployee.UI.Controllers
 {
     public class EmployeeController : Controller
     {
+        IEmployeeRepository _Employeerepo;
+
+        public EmployeeController(IEmployeeRepository Employeerepo)
+        {
+            this._Employeerepo = _Employeerepo;
+        }
         // GET: Employee
         public ActionResult Index()
         {
-            return View();
+            return View(_Employeerepo.GetEmployeeNs().ToList());
         }
     }
 }
